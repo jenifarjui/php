df760acad9d8c39469310657a33d591466706a32
Redwan-Hussain-Shawon <shawonredwanhussain2@gmail.com>
2024-06-03 22:05:52 +0600

    add

---
+++ registration-check.php
@@ -0,0 +1,12 @@
+<?php 
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+
+if(isset($_POST['registration_submit'])){
+    $name =htmlspecialchars($_POST['name']);
+    $email =htmlspecialchars($_POST['email']);
+    $password =htmlspecialchars($_POST['password']);
+    $number =htmlspecialchars($_POST['number']);
+}
+?>
---
+++ registration.php
@@ -0,0 +1,21 @@
+<?php
+include_once('include/header.php'); ?>
+
+
+<section id="registration">
+    <div class="container">
+        <form action="registration-check.php" method="post">
+            <h2>Registration On your account</h2>
+            <label for="name">Name</label>
+            <input type="text" id="name" placeholder="enter you name" name="name" required>
+            <label for="email">Email</label>
+            <input type="email" id="email" placeholder="enter you Email" name="email" required>
+            <label for="password">Password</label>
+            <input type="password" id="password" placeholder="enter you password" name="password" required>
+            <label for="number">Number</label>
+            <input type="text" id="number" placeholder="enter you number" name="number" required>
+            <button type="submit" name="registration_submit">Submit</button>
+        </form>
+    </div>
+</section>
+<?php include_once('include/footer.php'); ?>
